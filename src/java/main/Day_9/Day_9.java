1a5aa4623ebeb3ba703b0cc36e0485e8c6dc3e93
Matthew French <9086152+MatthewFrench@users.noreply.github.com>
2020-12-08 23:23:21 -0600

    Added comments where I tripped

--- src/java/main/Day_9/Day_9.java
+++ src/java/main/Day_9/Day_9.java
@@ -7,10 +7,12 @@ import static main.Utility.*;
 class Day_9 {
     public static void main(String[] args) throws Exception {
         log(Day_9.class.getName());
+        // Tripped here on making it load as a type. Then erroneously chose int over long
         final List<Long> input = loadTextFileAsTypeList(Day_9.class, "input.txt", Long::parseLong);
 
         var preamble = 25;
         long part1Answer = -1;
+        // Tripped here, starting at 0 instead of preamble
         for (var i = preamble; i < input.size(); i++) {
             var preambleList = input.subList(Math.max(i - preamble, 0), i);
             var number = input.get(i);
@@ -20,6 +22,7 @@ class Day_9 {
                 var number2 = preambleList.get(j);
                 for (var k = j + 1; k < preambleList.size(); k++) {
                     var number3 = preambleList.get(k);
+                    // Tripped here, put if they equaled to exit, actually opposite the case
                     if (!number2.equals(number3) && number2 + number3 == number) {
                         noNumber = false;
                         break inner;
@@ -40,6 +43,7 @@ class Day_9 {
             var smallestNumber = number;
             for (var j = i + 1; j < input.size(); j++) {
                 var number2 = input.get(j);
+                // Tripped on smallest/largest number, thought it was first/last
                 largestNumber = Math.max(largestNumber, number2);
                 smallestNumber = Math.min(smallestNumber, number2);
                 total += number2;
