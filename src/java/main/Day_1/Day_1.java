f2e291c2f7d9cdc54ef35ecb64323312e0ea4d74
Matthew French <9086152+MatthewFrench@users.noreply.github.com>
2021-10-22 22:18:57 -0500

    Changed to java 8

--- src/java/main/Day_1/Day_1.java
+++ src/java/main/Day_1/Day_1.java
@@ -7,18 +7,18 @@ class Day_1 {
         Utility.log("Day 1");
         final int targetValue = 2020;
         final List<Integer> input = Utility.loadTextFileAsTypeList(Day_1.class, "input.txt", Integer::parseInt);
-        for (var index1 = 0; index1 < input.size(); index1++) {
-            var number1 = input.get(index1);
-            for (var index2 = index1 + 1; index2 < input.size(); index2++) {
-                var number2 = input.get(index2);
+        for (int index1 = 0; index1 < input.size(); index1++) {
+            int number1 = input.get(index1);
+            for (int index2 = index1 + 1; index2 < input.size(); index2++) {
+                int number2 = input.get(index2);
                 if (number1 + number2 == targetValue) {
                     Utility.log("Solution 1");
                     Utility.log("Number 1: " + number1);
                     Utility.log("Number 2: " + number2);
                     Utility.log("Multiplied: " + (number1 * number2));
                 }
-                for (var index3 = index2 + 1; index3 < input.size(); index3++) {
-                    var number3 = input.get(index3);
+                for (int index3 = index2 + 1; index3 < input.size(); index3++) {
+                    int number3 = input.get(index3);
                     if (number1 + number2 + number3 == targetValue) {
                         Utility.log("Solution 2");
                         Utility.log("Number 1: " + number1);
